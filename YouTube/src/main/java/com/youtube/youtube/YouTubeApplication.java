1c56cb31519c69253facc7e0609026c6231677c3
Tereza <terezavl14@gmail.com>
2023-04-13 14:40:16 +0300

    Created Spring project

---
+++ YouTube/src/main/java/com/youtube/youtube/YouTubeApplication.java
@@ -0,0 +1,13 @@
+package com.youtube.youtube;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YouTubeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YouTubeApplication.class, args);
+	}
+
+}
